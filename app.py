d7826f0bbb3ac3ff98f382cc6ad4be7ed63d407e
Carlos Chinchilla <calufa@gmail.com>
2020-02-12 11:02:34 -0500

    add captions

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
 import json
+import yaml
 
 from config import *
 from flask import escape, Flask, render_template, request
-from story import load_story
+from google.cloud import storage
 
 
 app = Flask(__name__)
 
+storage_client = storage.Client()
+bucket = storage_client.get_bucket(BUCKET_NAME)
+
 
 @app.route('/', methods=['GET'])
 def index():
-	show_bg = request.args.get('show_bg', False)
 	story_id = request.args.get('story_id')
-	
-	story = load_story(story_id)
-	print(story)
+
+	blob_path = '{}/story.yaml'.format(story_id)
+	blob = bucket.get_blob(blob_path)
+	data = blob.download_as_string()
+	story = yaml.load(data, Loader=yaml.FullLoader)
 
 	date = escape('TODAY')
 	subtitle = escape(story['subtitle'])
 	title = [escape(part) for part in story['title']]
-	return render_template(HTML_TEMPLATE_FILENAME,
+	return render_template('512x1024.html',
 						   date=date,
 						   duration=story['duration'],
-						   show_bg=show_bg,
+						   story_id=story_id,
 						   subtitle=json.dumps(subtitle),
 						   title_parts=json.dumps(title))
+
+
+@app.route('/captions/<story_id>', methods=['GET'])
+def get_captions(story_id):
+	blob_path = '{}/captions.vtt'.format(story_id)
+	blob = bucket.get_blob(blob_path)
+	return blob.download_as_string()
